273a85426ffe205c6c1fae0dc2962c410a7a93bf
BassamBanyAli <bassambanyali@gmail.com>
2024-07-24 06:37:04 +0300

    Second_Task

---
+++ WebApplication8/WebApplication8/Controllers/SecondController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication8.Controllers
+{
+    public class SecondController : Controller
+    {
+        // GET: Second
+        public ActionResult Index()
+        {
+            return View();
+        }
+          public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult SubmitData(string name)
+        {
+            // Pass data back to the view using ViewBag
+            ViewBag.Name = name;
+            return View("Index");
+        }
+        public ActionResult SubmitDataContact(FormCollection form)
+        {
+            // Pass data back to the view using ViewBag
+            ViewBag.Name = form["name"];
+            ViewBag.Age = form["age"];
+            ViewBag.gender = form["gender"];
+            ViewBag.country = form["country"];
+            ViewBag.languages = form["languages"];
+
+            return View("Contact");
+        }
+    }
+}
---
+++ WebApplication8/WebApplication8/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication8.Models;
+
+namespace WebApplication8.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UserInfo()
+        {
+            var users = new List<User>
+            {
+                new User { Id = 1, Name = "dima", Email = "dima@gmail.com" },
+                new User { Id = 2, Name = "bassam", Email = "bassam@gmail.com" },
+                new User { Id = 3, Name = "suha", Email = "suha@gmail.com" },
+                new User { Id = 4, Name = "noor", Email = "noor@gmail.com" }
+            };
+
+            return View(users);
+        }
+    }
+}
